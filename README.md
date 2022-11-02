d307e3e3afa6a36e22336cd00001da0cec44fb7f
szuwgh <895340293@qq.com>
2022-11-02 11:21:02 +0800

    finish pg

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # dbsail
 This tool is used to connect to mysql or postgresql and insert data of customizable size
+
+
+##  User Guide
+
+#### insert mysql data
+```
+$ ./dbsail mysql -h 127.0.0.1 -u root -p 123456
+This tool is used to connect to mysql or postgresql and insert data of customizable size, power by wgh
+
+ ____    ____     ____    ______  ______   __
+/\  _`\ /\  _`\  /\  _`\ /\  _  \/\__  _\ /\ \
+\ \ \/\ \ \ \L\ \\ \,\L\_\ \ \L\ \/_/\ \/ \ \ \
+ \ \ \ \ \ \  _ <'\/_\__ \\ \  __ \ \ \ \  \ \ \  __
+  \ \ \_\ \ \ \L\ \ /\ \L\ \ \ \/\ \ \_\ \__\ \ \L\ \
+   \ \____/\ \____/ \ `\____\ \_\ \_\/\_____\\ \____/
+    \/___/  \/___/   \/_____/\/_/\/_/\/_____/ \/___/
+
+running dbsail mysql model
+
+connect mysql success 192.168.255.128:3306
+create data base testdbbase
+use data base testdbbase
+create table test1
+
+you will insert 10240 rows of data
+
+Processing ████████████████████████████████████████████████████████████████ 100%
+finish insert 10240 rows data, usage time 00:00:03
+```
+
+#### insert postgresql data
+
+```
+$ ./dbsail pg -h 127.0.0.1 -u test -p 123456
+This tool is used to connect to mysql or postgresql and insert data of customizable size, power by wgh
+
+ ____    ____     ____    ______  ______   __
+/\  _`\ /\  _`\  /\  _`\ /\  _  \/\__  _\ /\ \
+\ \ \/\ \ \ \L\ \\ \,\L\_\ \ \L\ \/_/\ \/ \ \ \
+ \ \ \ \ \ \  _ <'\/_\__ \\ \  __ \ \ \ \  \ \ \  __
+  \ \ \_\ \ \ \L\ \ /\ \L\ \ \ \/\ \ \_\ \__\ \ \L\ \
+   \ \____/\ \____/ \ `\____\ \_\ \_\/\_____\\ \____/
+    \/___/  \/___/   \/_____/\/_/\/_/\/_____/ \/___/
+
+running dbsail postgresql model
+
+connect postgresql success 127.0.0.1:5432 postgres
+create data base testdbbase
+connect postgresql success 127.0.0.1:5432 testdbbase
+create table test1
+
+you will insert 10240 rows of data
+
+Processing ████████████████████████████████████████████████████████████████ 100%
+finish insert 10240 rows data, usage time 00:00:02
+```
+
+#### clean data
+```
+$ ./dbsail mysql clean -h 127.0.0.1 -u test -p 123456
+$ ./dbsail pg clean -h 127.0.0.1 -u test -p 123456
+This tool is used to connect to mysql or postgresql and insert data of customizable size, power by wgh
+
+ ____    ____     ____    ______  ______   __
+/\  _`\ /\  _`\  /\  _`\ /\  _  \/\__  _\ /\ \
+\ \ \/\ \ \ \L\ \\ \,\L\_\ \ \L\ \/_/\ \/ \ \ \
+ \ \ \ \ \ \  _ <'\/_\__ \\ \  __ \ \ \ \  \ \ \  __
+  \ \ \_\ \ \ \L\ \ /\ \L\ \ \ \/\ \ \_\ \__\ \ \L\ \
+   \ \____/\ \____/ \ `\____\ \_\ \_\/\_____\\ \____/
+    \/___/  \/___/   \/_____/\/_/\/_/\/_____/ \/___/
+
+connect postgresql success 127.0.0.1:5432 postgres
+drop data base testdbbase
+```
+
+
+Commands:
+* `version`: show version
+* `pg`: use postgresql model
+* `mysql`: use mysql model
+* `clean`: drop database
+
+Common options
+
+* `-help`: Show dbsail's condensed help output.
+* `-h/--host`: database host address
+* `-u/--user`: database user account
+* `-P/--port`: database port
+* `-P/--password`: database user passport
+* `-b/--batch`: the number of rows inserted into the database at a time, ex: 10
+* `-c/--count`: the total number of rows inserted into the database, ex: 1000
+* `-s/--size`: total size of inserts into the database, ex:1K,1M,1G defalut:10M
--- dbsail.py
+++ dbsail.py
@@ -1,15 +1,12 @@
 import click
-import psycopg2
-import pymysql
-from pymysql.cursors import Cursor
-from pymysql.connections import Connection
+
 import os
 
 info = 'rWbhSHMi42ItE5GnqLgAQAHkb5PUsT1m7t8i3JzAteElBRqqDbzxKxwjUMi2aFM1VdjHLat4HV8KCqZQXD7Odi9hEWfbVowWjpWvKnxP8aRf93fdLBniNHDGQlVrYUzP9HbBNFthgapdKlbn78NwiyZkekKesTVhpHnHwDpAS71B7Lzrkz58pToEOgzWTEn8xvGeQirDFr7VFkQHev4tdiN4r8aID8n9Aqjf5QdvDHSIBdPy4VtpqSgWIF5XjfN'
 one_raw_size = 1  # 1KB
 v = '0.1'
-mysql_insert_sql = 'insert into test1 (FIRST_NAME,LAST_NAME, AGE, SEX, INFO, INCOME) values(%s,%s,%s,%s,%s,%s)'
-mysql_raw_data = ('Mac', 'Mohan', 20, 'M', info, 2000.11)
+insert_sql = 'insert into test1 (FIRST_NAME,LAST_NAME, AGE, SEX, INFO, INCOME) values(%s,%s,%s,%s,%s,%s)'
+raw_data = ('Mac', 'Mohan', 20, 'M', info, 2000.11)
 
 
 def exit():
@@ -31,6 +28,11 @@ def size_to_int(size: str):
     elif d == 'G' or d == 'g':
         s = i * 1024 * 1024
     return s
+
+
+import pymysql
+from pymysql.cursors import Cursor
+from pymysql.connections import Connection
 
 
 class Mysql:
@@ -84,6 +86,7 @@ class Mysql:
 
     # 关闭数据库
     def close(self):
+        self.cur.close()
         self.db.close()
 
     # 插入数据
@@ -102,17 +105,154 @@ class Mysql:
                           max=q + 1,
                           suffix='%(percent)d%%',
                           width=64)
+        print("you will insert %s rows of data\n" % count)
         for _ in range(0, q):
             values = []
             for _ in range(0, batch):
-                values.append(mysql_raw_data)
-            self.executemany(mysql_insert_sql, values)
+                values.append(raw_data)
+            self.executemany(insert_sql, values)
             bar.next()
         if r > 0:
             values = []
             for _ in range(0, r):
-                values.append(mysql_raw_data)
-            self.executemany(mysql_insert_sql, values)
+                values.append(raw_data)
+            self.executemany(insert_sql, values)
+        bar.next()
+        bar.finish()
+        ticks2 = time.time()
+        st = strftime("%H:%M:%S", gmtime(ticks2 - ticks1))
+        print("finish insert %s rows data, usage time %s" % (count, st))
+
+    def executemany(self, sql, values):
+        try:
+            # 执行sql语句
+            self.cur.executemany(sql, values)
+            # 提交到数据库执行
+
+            self.db.commit()
+
+        except Exception as e:
+            # 如果发生错误则回滚
+            self.db.rollback()
+            print('execute sql fail:', sql, e)
+            self.db.close
+            exit()
+
+    def execute(self, sql):
+        try:
+            # 执行sql语句
+            self.cur.execute(sql)
+            # 提交到数据库执行
+            self.db.commit()
+        except Exception as e:
+            # 如果发生错误则回滚
+            self.db.rollback()
+            print('execute sql fail:', sql, e)
+            self.close()
+            exit()
+
+
+import psycopg2
+from psycopg2._psycopg import connection as pgconn
+from psycopg2._psycopg import cursor as pgcur
+
+
+class Postgresql:
+    db: pgconn
+    cur: pgcur
+    user: str
+
+    def __init__(self, host, user, port, password, database):
+        try:
+            self.user = user
+            self.db = psycopg2.connect(
+                database=database,
+                host=host,
+                user=user,
+                port=port,
+                password=password,
+            )
+            self.db.autocommit = True
+            self.cur = self.db.cursor()
+
+        except Exception as e:
+            print('connect postgresql fail:', e)
+            exit()
+        print("connect postgresql success",
+              "{}:{} {}".format(host, port, database))
+
+    # 创建数据库
+    def create_data_base(self, data_base):
+        exist_sql = "SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{}'".format(
+            data_base)
+        create_sql = "CREATE DATABASE {} OWNER {}".format(
+            data_base,
+            self.user,
+        )
+        self.execute(exist_sql)
+        exists = self.cur.fetchone()
+        if not exists:
+            self.execute(create_sql)
+        print("create data base %s" % data_base)
+
+    # 删除数据库
+    def delete_data_base(self, data_base):
+        sql = "DROP DATABASE {}".format(data_base)
+        self.execute(sql)
+        print("drop data base %s" % data_base)
+
+    # 使用哪个数据库
+    # def use(self, data_base):
+    #     sql = "\\c {}".format(data_base)
+    #     self.execute(sql)
+    #     print("You are now connected to database %s as user postgres %s" %
+    #           (data_base, self.user))
+
+    # 创建数据库表
+    def create_table(self):
+        sql = """CREATE TABLE if not exists test1 (
+         id serial PRIMARY KEY NOT NULL,
+         FIRST_NAME  VARCHAR(255) NOT NULL,
+         LAST_NAME  VARCHAR(255),
+         AGE int,  
+         SEX VARCHAR(247),
+         INFO VARCHAR(255),
+         INCOME FLOAT )"""
+        self.execute(sql)
+        print("create table test1\n")
+
+    # 关闭数据库
+    def close(self):
+        self.db.close()
+
+    # 插入数据
+    def insert_data(self, batch, count, size):
+        s = int(size_to_int(size) / one_raw_size)
+        if count == 0:
+            count = s
+        import time
+        from time import strftime
+        from time import gmtime
+        ticks1 = time.time()
+        from progress.bar import ChargingBar
+        q = int(count / batch)
+        r = count % batch
+        bar = ChargingBar('Processing',
+                          max=q + 1,
+                          suffix='%(percent)d%%',
+                          width=64)
+        print("you will insert %s rows of data\n" % count)
+        for _ in range(0, q):
+            values = []
+            for _ in range(0, batch):
+                values.append(raw_data)
+            self.executemany(insert_sql, values)
+            bar.next()
+        if r > 0:
+            values = []
+            for _ in range(0, r):
+                values.append(raw_data)
+            self.executemany(insert_sql, values)
         bar.next()
         bar.finish()
         ticks2 = time.time()
@@ -151,19 +291,16 @@ class Mysql:
 @click.group()  #将main函数装饰成一个group对象
 def main():
     print(
-        '\nThis tool is used to connect to mysql or postgresql and insert data of customizable size, power by szuwgh'
+        '\nThis tool is used to connect to mysql or postgresql and insert data of customizable size, power by wgh'
     )
     print('''
-      ___           ___           ___           ___                       ___ 
-     /\  \         /\  \         /\  \         /\  \          ___        /\__\\
-    /::\  \       /::\  \       /::\  \       /::\  \        /\  \      /:/  /
-   /:/\:\  \     /:/\:\  \     /:/\ \  \     /:/\:\  \       \:\  \    /:/  / 
-  /:/  \:\__\   /::\~\:\__\   _\:\~\ \  \   /::\~\:\  \      /::\__\  /:/  /  
- /:/__/ \:|__| /:/\:\ \:|__| /\ \:\ \ \__\ /:/\:\ \:\__\  __/:/\/__/ /:/__/   
- \:\  \ /:/  / \:\~\:\/:/  / \:\ \:\ \/__/ \/__\:\/:/  / /\/:/  /    \:\  \   
-  \:\  /:/  /   \:\ \::/  /   \:\ \:\__\        \::/  /  \::/__/      \:\  \  
-   \:\/:/  /     \:\/:/  /     \:\/:/  /        /:/  /    \:\__\       \:\  \ 
-    \__/__/       \__/__/       \__/__/        /_/__/      \/__/        \_\__\\
+ ____    ____     ____    ______  ______   __        
+/\  _`\ /\  _`\  /\  _`\ /\  _  \/\__  _\ /\ \       
+\ \ \/\ \ \ \L\ \\\\ \,\L\_\ \ \L\ \/_/\ \/ \ \ \      
+ \ \ \ \ \ \  _ <'\/_\__ \\\\ \  __ \ \ \ \  \ \ \  __ 
+  \ \ \_\ \ \ \L\ \ /\ \L\ \ \ \/\ \ \_\ \__\ \ \L\ \\
+   \ \____/\ \____/ \ `\____\ \_\ \_\/\_____\\\\ \____/
+    \/___/  \/___/   \/_____/\/_/\/_/\/_____/ \/___/ 
     ''')
     pass
 
@@ -187,7 +324,7 @@ def main():
               help='mysql passport')
 @click.option('--database',
               '-d',
-              default='kmetest',
+              default='testdbbase',
               type=str,
               help='mysql database')
 @click.option('--batch',
@@ -205,15 +342,26 @@ def main():
               default='10M',
               type=str,
               help='insert size ex: 1K,1M,1G')
+@click.option('--singlesize',
+              '-ss',
+              default='1M',
+              type=str,
+              help='Maximum data in a single table ex: 1K,1M,1G')
 @click.pass_context
-def mysql(ctx, host, user, port, password, database, batch, count, size):
+def mysql(ctx, host, user, port, password, database, batch, count, size,
+          singlesize):
     if ctx.invoked_subcommand is None:
         print('running dbsail mysql model\n')
         db = Mysql(host=host, user=user, port=port, password=password)
+        # 创建数据库
         db.create_data_base(database)
+        # 使用数据库
         db.use(database)
+        # 创建表
         db.create_table()
+        # 插入数据
         db.insert_data(batch, count, size)
+        # 关闭数据库连接
         db.close()
     pass
 
@@ -237,7 +385,7 @@ def mysql(ctx, host, user, port, password, database, batch, count, size):
               help='mysql passport')
 @click.option('--database',
               '-d',
-              default='kmetest',
+              default='testdbbase',
               type=str,
               help='mysql database')  #mysql clean 子命令
 def mysql_clean(
@@ -254,18 +402,114 @@ def mysql_clean(
 
 
 @click.group(invoke_without_command=True)  #将下面函数装饰成命令0
-@click.option('--name', '--a', default='ZJW', type=str, help='skfjkskfs name.')
+@click.option('--host',
+              '-h',
+              default='localhost',
+              type=str,
+              help='postgresql host address')
+@click.option('--user',
+              '-u',
+              default='root',
+              type=str,
+              help='postgresql user account')
+@click.option('--port', '-P', default=5432, type=int, help='postgresql port')
+@click.option('--password',
+              '-p',
+              default='xxx',
+              type=str,
+              help='postgresql passport')
+@click.option('--database',
+              '-d',
+              default='testdbbase',
+              type=str,
+              help='postgresql database')
+@click.option('--batch',
+              '-b',
+              default=10,
+              type=int,
+              help='batch insert count,ex: 10')
+@click.option('--count',
+              '-c',
+              default=0,
+              type=int,
+              help='insert count ex: 1000')
+@click.option('--size',
+              '-s',
+              default='10M',
+              type=str,
+              help='insert size ex: 1K,1M,1G')
+@click.option('--singlesize',
+              '-ss',
+              default='1M',
+              type=str,
+              help='Maximum data in a single table ex: 1K,1M,1G')
 @click.pass_context
-def pg(ctx, name):
+def pg(ctx, host, user, port, password, database, batch, count, size,
+       singlesize):
     if ctx.invoked_subcommand is None:
-        print("xx")
+        print('running dbsail postgresql model\n')
+        db = Postgresql(host=host,
+                        user=user,
+                        port=port,
+                        password=password,
+                        database='postgres')
+        # 创建数据库
+        db.create_data_base(database)
+        db.close()
+
+        # 重新连接
+        db = Postgresql(host=host,
+                        user=user,
+                        port=port,
+                        password=password,
+                        database=database)
+
+        # 创建表
+        db.create_table()
+
+        # 插入数据
+        db.insert_data(batch, count, size)
+
+        db.close()
     pass
 
 
 @click.command(name='clean')
-@click.option('--debug', default=False)
-def pg_clean(debug):
-    print("use pg clean")
+@click.option('--host',
+              '-h',
+              default='localhost',
+              type=str,
+              help='postgresql host address')
+@click.option('--user',
+              '-u',
+              default='root',
+              type=str,
+              help='postgresql user account')
+@click.option('--port', '-P', default=5432, type=int, help='postgresql port')
+@click.option('--password',
+              '-p',
+              default='xxx',
+              type=str,
+              help='postgresql passport')
+@click.option('--database',
+              '-d',
+              default='testdbbase',
+              type=str,
+              help='postgresql database')  #mysql clean 子命令
+def pg_clean(
+    host,
+    user,
+    port,
+    password,
+    database,
+):
+    db = Postgresql(host=host,
+                    user=user,
+                    port=port,
+                    password=password,
+                    database="postgres")
+    db.delete_data_base(database)
+    db.close()
 
 
 @click.command(name='version')
